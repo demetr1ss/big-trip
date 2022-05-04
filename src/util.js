148f6a734d7e46382263967a58966dabf326b088
Dmitry_T <cheko.as@inbox.ru>
2022-05-04 17:25:58 +0300

    Форматирование продолжительности ивента

--- src/util.js
+++ src/util.js
@@ -14,10 +14,33 @@ export const humanizeTime = (date) => dayjs(date).format('HH:mm');
 export const getDurationTime = (dateFrom, dateTo) => {
   dateTo = dayjs(dateTo).format('YYYY-MM-DDTHH:mm');
   let minutes = dayjs(dateTo).diff(dateFrom, 'minutes', true);
-  const hours = Math.floor(minutes / 60);
+  let hours = Math.floor(minutes / 60);
   minutes = Math.ceil(minutes - (hours * 60));
 
-  return `${hours}H ${minutes}M`;
+  if (minutes === 60) {
+    hours++;
+    minutes = 0;
+  }
 
+  if (`${minutes}`.length < 2) {
+    minutes = `0${minutes}`;
+  }
+
+  if (`${hours}`.length < 2) {
+    hours = `0${hours}`;
+  }
+
+  if (hours >= 24) {
+    let days = Math.floor(hours / 24);
+    hours = hours - (days * 24);
+
+    if (`${days}`.length < 2) {
+      days = `0${days}`;
+    }
+
+    return `${days}D ${hours}H ${minutes}M`;
+  }
+
+  return `${hours}H ${minutes}M`;
 };
 
